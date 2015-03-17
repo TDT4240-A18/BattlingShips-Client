5e4a8424303c21a9ff777783d6acb18fa380fd52
EirikZimmerWold <eirikzimmer@hotmail.com>
2015-03-17 13:26:19 +0100

    updated gamerules interface
    added a couple of methods

--- app/src/main/java/no/ntnu/tdt4240/a18/battlingships/model/Gamerules.java
+++ app/src/main/java/no/ntnu/tdt4240/a18/battlingships/model/Gamerules.java
@@ -4,4 +4,16 @@ package no.ntnu.tdt4240.a18.battlingships.model;
  * Created by Ondra on 10/03/15.
  */
 public interface Gamerules {
+
+    //checks if boat tries to leave board (might need to change posX and posY to something else)
+    public boolean checkMove(Ship ship, int posX,int posY);
+
+    //checks if boat tries to shoot outside the board (might need to change posX and posY to something else)
+    public boolean checkShot(Ship ship, int posX,int posY);
+
+    //check the list of players and their ships life to see if one has won
+    public boolean checkWin();
+
+    //checks if the position you try to move to is occupied by another ship
+    public boolean checkCollide(Ship ship, int posX,int posY);
 }

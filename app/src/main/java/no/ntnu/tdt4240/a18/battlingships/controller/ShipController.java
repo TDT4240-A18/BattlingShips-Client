edb6b0daff2bfa0ecb111ba17e074be0514c4fce
EirikZimmerWold <eirikzimmer@hotmail.com>
2015-04-09 18:39:22 +0200

    basicgame -> gamerules
    just a minor change in variables

--- app/src/main/java/no/ntnu/tdt4240/a18/battlingships/controller/ShipController.java
+++ app/src/main/java/no/ntnu/tdt4240/a18/battlingships/controller/ShipController.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import no.ntnu.tdt4240.a18.battlingships.model.Board;
 import no.ntnu.tdt4240.a18.battlingships.model.Game;
+import no.ntnu.tdt4240.a18.battlingships.model.Gamerules;
 import no.ntnu.tdt4240.a18.battlingships.model.NetworkInterface;
 import no.ntnu.tdt4240.a18.battlingships.model.Player;
 import no.ntnu.tdt4240.a18.battlingships.model.ruleset.BasicGame;
@@ -15,7 +16,7 @@ public class ShipController extends Application {
 
     private Player player = new Player();
     private Board board = new Board();
-    private BasicGame rules = new BasicGame(board);
+    private Gamerules rules = new BasicGame(board);
     private Game game = new Game(rules, board);
     private NetworkInterface net = new NetworkInterface();
 

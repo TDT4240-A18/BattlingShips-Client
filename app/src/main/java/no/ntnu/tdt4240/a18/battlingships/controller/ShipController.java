9d9aa2db1c49a35db6317bdf6bc317bf3e6e9372
EirikZimmerWold <eirikzimmer@hotmail.com>
2015-04-09 18:23:16 +0200

    shipcontroller extends application

--- app/src/main/java/no/ntnu/tdt4240/a18/battlingships/controller/ShipController.java
+++ app/src/main/java/no/ntnu/tdt4240/a18/battlingships/controller/ShipController.java
@@ -1,15 +1,35 @@
 package no.ntnu.tdt4240.a18.battlingships.controller;
 
-import no.ntnu.tdt4240.a18.battlingships.model.Ship;
+import android.app.Application;
+
+import no.ntnu.tdt4240.a18.battlingships.model.Board;
+import no.ntnu.tdt4240.a18.battlingships.model.Game;
+import no.ntnu.tdt4240.a18.battlingships.model.NetworkInterface;
+import no.ntnu.tdt4240.a18.battlingships.model.Player;
+import no.ntnu.tdt4240.a18.battlingships.model.ruleset.BasicGame;
 
 /**
  * Created by Ondra on 11/03/15.
  */
-public class ShipController {
+public class ShipController extends Application {
 
-    private final Ship ship;
+    private Player player = new Player();
+    private Board board = new Board();
+    private BasicGame rules = new BasicGame(board);
+    private Game game = new Game(rules, board);
+    private NetworkInterface net = new NetworkInterface();
 
-    public ShipController(Ship ship) {
-        this.ship = ship;
+    public Player getPlayer(){
+        return player;
+    }
+    public Game getGame(){
+        return game;
+    }
+    public Board getBoard(){
+        return board;
+    }
+    public NetworkInterface getNet(){
+        return net;
     }
 }
+

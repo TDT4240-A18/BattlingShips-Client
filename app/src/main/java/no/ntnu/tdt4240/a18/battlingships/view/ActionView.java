6809cb942936e2d6a41f0ba4977ee9188d8059c4
SondrePadoy <sondre.padoey@live.com>
2015-04-14 14:47:36 +0200

    ActionView
    Added functions to all buttons in ActionView

--- app/src/main/java/no/ntnu/tdt4240/a18/battlingships/view/ActionView.java
+++ app/src/main/java/no/ntnu/tdt4240/a18/battlingships/view/ActionView.java
@@ -6,32 +6,109 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import no.ntnu.tdt4240.a18.battlingships.R;
 import no.ntnu.tdt4240.a18.battlingships.controller.ShipController;
 
+
 public class ActionView extends ActionBarActivity {
     private boolean shoot=false;
     private boolean move=false;
+    private Button ulButton = (Button) findViewById(R.id.ulButton);
+    private Button upButton = (Button) findViewById(R.id.upButton);
+    private Button urButton= (Button) findViewById(R.id.urButton);
+    private Button leftButton= (Button) findViewById(R.id.leftButton);
+    private Button cancelButton= (Button) findViewById(R.id.cancelButton);
+    private Button rightButton= (Button) findViewById(R.id.rightButton);
+    private Button dlButton= (Button) findViewById(R.id.dlbutton);
+    private Button downButton= (Button) findViewById(R.id.downButton);
+    private Button drButton= (Button) findViewById(R.id.drButton);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_view);
         final ShipController aController = (ShipController) getApplicationContext();
+        move=false;
+        shoot=false;
     }
     public void mapView(View view){
         Intent intent = new Intent(this, MapView.class);
         startActivity(intent);
 
     }
-    public void shoot(View view){
+    public void shoot(){
         shoot=true;
         move=false;
-
-
-
+        ulButton.setText("UpperLeft");
+        upButton.setText("Up");
+        urButton.setText("UpperRight");
+        leftButton.setText("Left");
+        rightButton.setText("Right");
+        dlButton.setText("DownLeft");
+        downButton.setText("Down");
+        drButton.setText("DownRight");
+        cancelButton.setText("Cancel");
     }
+    public void move(){
+        shoot=false;
+        move=true;
+        ulButton.setText("UpperLeft");
+        upButton.setText("Up");
+        urButton.setText("UpperRight");
+        leftButton.setText("Left");
+        rightButton.setText("Right");
+        dlButton.setText("DownLeft");
+        downButton.setText("Down");
+        drButton.setText("DownRight");
+        cancelButton.setText("Cancel");
+    }
+    public void cancel(){
+        if(shoot||move){
+            shoot=false;
+            move=false;
+            ulButton.setText("");
+            upButton.setText("");
+            urButton.setText("");
+            leftButton.setText("");
+            rightButton.setText("");
+            dlButton.setText("");
+            downButton.setText("");
+            drButton.setText("");
+            cancelButton.setText("");
+        }
+    }
+    //Doing actions
+    /*
+    public void upLeft(){
+        action(-1,-1, shoot, move);
+    }
+    public void upRight(){
+        action(1,-1, shoot, move);
+    }
+    public void up(){
+        action(0,-1, shoot, move);
+    }
+    public void left(){
+        action(-1,0, shoot, move);
+    }
+    public void right(){
+        action(1,0, shoot, move);
+    }
+    public void downLeft(){
+        action(-1,1, shoot, move);
+    }
+    public void downRight(){
+        action(1,1, shoot, move);
+    }
+    public void down(){
+        action(0,1, shoot, move);
+    }
+    public void idle(){
+        action(0,0,false,false);
+    }*/
 
 
 
